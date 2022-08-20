4a9d733ca6bc99887499f42e58d74f35dc263801
ozlem <ozlem@OZLEM>
2022-08-20 15:33:21 +0300

    Add project files.

---
+++ MvcStockTracking/Controllers/KategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcStockTracking.Controllers
+{
+    public class KategoryController : Controller
+    {
+        // GET: Kategory
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
